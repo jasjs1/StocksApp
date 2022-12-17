ce50dbdb09e62da6b1acf62324314a6777fd3288
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-16 20:17:50 -0600

    Create MSFTStockTemplate.swift

---
+++ MSFTStockTemplate.swift
@@ -0,0 +1,53 @@
+//
+//  MSFTStockTemplate.swift
+//  Stocks
+//
+//  Created by Jayce Sagvold on 12/16/22.
+//
+
+import SwiftUI
+
+struct MSFTStockTemplate: View {
+    var body: some View {
+        
+        
+        
+        // Copy this code and make a new file and change the "ph"
+        
+        
+        VStack {
+            
+            
+            //          Text("---------------------------------------------------")
+            //        .foregroundColor(.secondary)
+            //       .padding(.bottom, 2.5)
+            
+            HStack {
+                
+                Text("Microsoft - Microsoft.inc")
+                    .padding(.horizontal, 50)
+                
+                Image("StockGraph")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                
+            }
+            
+            VStack {
+                
+                Text("---------------------------------------------------")
+                    .foregroundColor(.secondary)
+                    .padding(.top, 2.5)
+            }
+            
+            
+            
+        }
+    }
+    
+    struct MSFTStockTemplate_Previews: PreviewProvider {
+        static var previews: some View {
+            MSFTStockTemplate()
+        }
+    }
+}
